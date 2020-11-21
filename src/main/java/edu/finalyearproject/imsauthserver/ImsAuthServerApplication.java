5e90da32b0688bc2940e787895ca25bbafc74e8e
Alec <rcsmith.alec@gmail.com>
2020-11-21 20:25:26 +0000

    project setup

---
+++ src/main/java/edu/finalyearproject/imsauthserver/ImsAuthServerApplication.java
@@ -0,0 +1,15 @@
+package edu.finalyearproject.imsauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsAuthServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ImsAuthServerApplication.class, args);
+    }
+
+}
